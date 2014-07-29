f08b96dd26dcaffdb2bec5e957febe9365b217c1
David Hackro <hackro91@gmail.com>
2014-07-28 20:30:27 -0500

    Create conversor.java

---
+++ conversor.java
@@ -0,0 +1,9 @@
+ double Celsius = -17.2;//un fahrenheit a celsius
+        double fahrenheit = 33.8;//celsius a farenheit
+        
+        double C = 3;//cantidad ingresada caso a
+        double F = 1;//cantidad ingresada caso b
+        
+       
+        System.out.println(" coonversion de Celsius a Fihrenheit : "+(C * fahrenheit));
+        System.out.println(" coonversion de  Fihrenheit a Celsius : "+(F * Celsius));
