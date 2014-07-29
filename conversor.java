76e3b6fc4e0758dec5b11110bca2d7ec50ea98a4
David Hackro <hackro91@gmail.com>
2014-07-28 21:03:02 -0500

    Update conversor.java

--- conversor.java
+++ conversor.java
@@ -1,9 +1,16 @@
- double Celsius = -17.2;//un fahrenheit a celsius
+        double Celsius = -17.2;//un fahrenheit a celsius
         double fahrenheit = 33.8;//celsius a farenheit
         
-        double C = 3;//cantidad ingresada caso a
-        double F = 1;//cantidad ingresada caso b
+        double C = 3;//Aqui leemos lo que hay en la caja de texto de los celsius
+       
         
        
-        System.out.println(" conversion de Celsius a Fihrenheit : "+(C * fahrenheit));
-        System.out.println(" conversion de  Fihrenheit a Celsius : "+(F * Celsius));
+       caja_Fihrenheit.setext = C*fahrenheit;//asignamos el valor a la caja de fahrenheit
+       
+       
+       
+       
+       
+        //double F = 1;//cantidad ingresada caso b
+        //System.out.println(" conversion de Celsius a Fihrenheit : "+(C * fahrenheit));
+        //System.out.println(" conversion de  Fihrenheit a Celsius : "+(F * Celsius));
